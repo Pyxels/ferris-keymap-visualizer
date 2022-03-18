f8cad72b648dc728c8ef37d08d2288876c6253cf
Pyxels <39232833+Pyxels@users.noreply.github.com>
2022-03-18 15:55:30 +0100

    Create a key struct, methods and functions

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,63 @@
 use svg::node;
-use svg::node::element::{Rectangle, Text};
+use svg::node::element::{Group, Rectangle, Text};
 use svg::Document;
 
+const KEY_FILL_COL: &str = "#3f403f";
+const KEY_BORDER_COLOR: &str = "#303030";
+const KEY_BORDER_SIZE: i16 = 6;
+const KEY_DIMENSIONS: i16 = 100;
+const KEY_BORDER_RADIUS: i16 = 20;
+const TEXT_COLOR: &str = "white";
+const TEXT_SIZE: &str = "5em";
+
+struct Key {
+    label: String,
+    x: i16,
+    y: i16,
+    mod_label: Option<String>,
+}
+
+impl Key {
+    fn new(x: i16, y: i16, label: String) -> Self {
+        Key {
+            label,
+            x,
+            y,
+            mod_label: None,
+        }
+    }
+    fn add_mod_label(&mut self, label: String) {
+        self.mod_label = Some(label);
+    }
+    fn svg(&self) -> Group {
+        let key = Rectangle::new()
+            .set("fill", KEY_FILL_COL)
+            .set("stroke", KEY_BORDER_COLOR)
+            .set("stroke-width", KEY_BORDER_SIZE)
+            .set("x", self.x)
+            .set("y", self.y)
+            .set("rx", KEY_BORDER_RADIUS)
+            .set("ry", KEY_BORDER_RADIUS)
+            .set("width", KEY_DIMENSIONS)
+            .set("height", KEY_DIMENSIONS);
+
+        let letter = Text::new()
+            .set("x", self.x + KEY_DIMENSIONS / 3)
+            .set("y", self.y + KEY_DIMENSIONS / 2)
+            .set("fill", TEXT_COLOR)
+            .set("font-size", TEXT_SIZE)
+            .add(node::Text::new(&self.label));
+
+        Group::new().add(key).add(letter)
+    }
+}
+
 fn main() {
+    let key = Key::new(20, 20, "A".to_string());
 
-    let key = Rectangle::new()
-        .set("fill", "#3f403f")
-        .set("stroke", "#303030")
-        .set("stroke-width", 6_i16)
-        .set("x", 20_i16)
-        .set("y", 10_i16)
-        .set("rx", 20_i16)
-        .set("ry", 20_i16)
-        .set("width", 100_i16)
-        .set("height", 100_i16);
+    let group = key.svg();
 
-    let letter = Text::new()
-        .set("x", 50)
-        .set("y", 80)
-        .set("fill", "white")
-        .set("font-size", "5em")
-        .add(node::Text::new("T"));
-
-
-    let document = Document::new().set("viewBox", (0, 0, 200, 200)).add(key).add(letter);
+    let document = Document::new().set("viewBox", (0, 0, 200, 200)).add(group);
 
     svg::save("image.svg", &document).unwrap();
 }
